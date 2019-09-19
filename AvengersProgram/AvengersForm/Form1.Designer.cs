5f98d64d4aa612afc7c3440e7f687bf874d7e98a
Guy-Kaplan <39667435+Guy-Kaplan@users.noreply.github.com>
2019-09-19 18:40:29 +0300

    Adding main program

---
+++ AvengersProgram/AvengersForm/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace AvengersForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.CharactersComboBox = new System.Windows.Forms.ComboBox();
+            this.FullNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RandomButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ShowAllButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(21, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a character:";
+            // 
+            // CharactersComboBox
+            // 
+            this.CharactersComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CharactersComboBox.FormattingEnabled = true;
+            this.CharactersComboBox.Items.AddRange(new object[] {
+            "Captain America",
+            "Falcon",
+            "Iron Man",
+            "Black Widow",
+            "Hawkeye",
+            "Hulk",
+            "Scarlet Witch",
+            "War Machine",
+            "Ant-Man",
+            "Wasp",
+            "Winter Soldier",
+            "Spider-Man",
+            "Doctor Strange",
+            "Black Panther",
+            "Captain Marvel"});
+            this.CharactersComboBox.Location = new System.Drawing.Point(200, 49);
+            this.CharactersComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CharactersComboBox.Name = "CharactersComboBox";
+            this.CharactersComboBox.Size = new System.Drawing.Size(207, 24);
+            this.CharactersComboBox.TabIndex = 2;
+            this.CharactersComboBox.SelectedIndexChanged += new System.EventHandler(this.CharactersComboBox_SelectedIndexChanged);
+            // 
+            // FullNameTextBox
+            // 
+            this.FullNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FullNameTextBox.Location = new System.Drawing.Point(200, 122);
+            this.FullNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FullNameTextBox.MaxLength = 30;
+            this.FullNameTextBox.Name = "FullNameTextBox";
+            this.FullNameTextBox.ReadOnly = true;
+            this.FullNameTextBox.Size = new System.Drawing.Size(207, 26);
+            this.FullNameTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(52, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Real Name:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 319);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(97, 486);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Made by Guy Kaplan";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RandomButton
+            // 
+            this.RandomButton.BackColor = System.Drawing.Color.ForestGreen;
+            this.RandomButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RandomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.RandomButton.ForeColor = System.Drawing.Color.White;
+            this.RandomButton.Location = new System.Drawing.Point(31, 198);
+            this.RandomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RandomButton.Name = "RandomButton";
+            this.RandomButton.Size = new System.Drawing.Size(172, 44);
+            this.RandomButton.TabIndex = 7;
+            this.RandomButton.Text = "Random";
+            this.RandomButton.UseVisualStyleBackColor = false;
+            this.RandomButton.Click += new System.EventHandler(this.RandomButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Crimson;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(235, 257);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(172, 44);
+            this.ExitButton.TabIndex = 8;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ClearButton.ForeColor = System.Drawing.Color.White;
+            this.ClearButton.Location = new System.Drawing.Point(31, 257);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(172, 44);
+            this.ClearButton.TabIndex = 9;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ShowAllButton
+            // 
+            this.ShowAllButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.ShowAllButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ShowAllButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ShowAllButton.ForeColor = System.Drawing.Color.White;
+            this.ShowAllButton.Location = new System.Drawing.Point(235, 198);
+            this.ShowAllButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ShowAllButton.Name = "ShowAllButton";
+            this.ShowAllButton.Size = new System.Drawing.Size(172, 44);
+            this.ShowAllButton.TabIndex = 10;
+            this.ShowAllButton.Text = "Show all";
+            this.ShowAllButton.UseVisualStyleBackColor = false;
+            this.ShowAllButton.Click += new System.EventHandler(this.ShowAllButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 536);
+            this.Controls.Add(this.ShowAllButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.RandomButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FullNameTextBox);
+            this.Controls.Add(this.CharactersComboBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Avengers Database";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CharactersComboBox;
+        private System.Windows.Forms.TextBox FullNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button RandomButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ShowAllButton;
+    }
+}
+
---
+++ AvengersProgram/AvengersForm/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AvengersForm
+{
+    public partial class Form1 : Form
+    {
+        // Made by Guy Kaplan
+
+        // List of all characters
+        private static readonly List<string> charactersList = new List<string>
+            {
+                "Captain America", "Falcon", "Iron Man", "Black Widow",
+                "Hawkeye", "Hulk", "Scarlet Witch", "War Machine",
+                "Ant-Man", "Wasp", "Winter Soldier", "Spider-Man",
+                "Doctor Strange", "Black Panther", "Captain Marvel",
+            };
+        private static readonly Random Random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Displays the chosen character's full name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CharactersComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayFullName();
+        }
+
+        /// <summary>
+        /// Selects a random character and displays its full name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RandomButton_Click(object sender, EventArgs e)
+        {
+            CharactersComboBox.Text = GetRndCharacterName();
+            DisplayFullName();
+        }
+
+        private void ShowAllButton_Click(object sender, EventArgs e)
+        {
+            var sb = new StringBuilder();
+            foreach (var character in charactersList)
+            {
+                sb.AppendLine($"{character} = {GetRealNameOfGivenCharacter(character)}");
+            }
+            MessageBox.Show(sb.ToString(), "All characters and real names");
+        }
+
+        /// <summary>
+        /// Resets both text fields
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            CharactersComboBox.Text = FullNameTextBox.Text = string.Empty;
+            CharactersComboBox.ResetText();
+            CharactersComboBox.SelectedIndex = -1;
+        }
+
+        /// <summary>
+        /// Exits the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?",
+                "Exit the program", MessageBoxButtons.YesNoCancel)
+                == DialogResult.Yes)
+            {
+                Close(); // exit the program
+            }
+        }
+
+        /// <summary>
+        /// Gets a random character name
+        /// </summary>
+        /// <returns>A random character name</returns>
+        private string GetRndCharacterName()
+        {
+            return charactersList[Random.Next(charactersList.Count)];
+        }
+
+        /// <summary>
+        /// Displays the chosen character's real name
+        /// </summary>
+        private void DisplayFullName()
+        {
+            FullNameTextBox.Text = GetRealNameOfGivenCharacter(CharactersComboBox.Text);
+        }
+
+        /// <summary>
+        /// Gets the rael name of the given character
+        /// </summary>
+        /// <param name="nickname">The given character</param>
+        /// <returns>The the rael name of the given character</returns>
+        private string GetRealNameOfGivenCharacter(string nickname)
+        {
+            switch (nickname)
+            {
+                case "Captain America":
+                    return "Steve Rogers";
+                case "Falcon":
+                    return "Sam Wilson";
+                case "Iron Man":
+                    return "Tony Stark";
+                case "Black Widow":
+                    return "Natasha Romanoff";
+                case "Hawkeye":
+                    return "Clint Barton";
+                case "Hulk":
+                    return "Dr. Bruce Banner";
+                case "Scarlet Witch":
+                    return "Wanda Maximoff";
+                case "War Machine":
+                    return "Rupert 'Rhodey' Rhodes";
+                case "Ant-Man":
+                    return "Scott Lang";
+                case "Wasp":
+                    return "Hope Van Dyne";
+                case "Winter Soldier":
+                    return "James 'Bucky' Barnes";
+                case "Spider-Man":
+                    return "Peter Parker";
+                case "Doctor Strange":
+                    return "Stephen Vincent Strange";
+                case "Black Panther":
+                    return "T'Challa";
+                case "Captain Marvel":
+                    return "Carol Danvers";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
